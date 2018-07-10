332dab7b37e14dbbfde47b56992801d097a215b9
AdamSmasherDerby <danielalt7@gmail.com>
2018-07-10 15:59:39 -0400

    Mostly functional at this point

--- html/views/wb/penaltydisplay.js
+++ html/views/wb/penaltydisplay.js
@@ -1,26 +1,22 @@
 $(initialize);
 
-var penaltyEditor = null;
 var period = null;
 var jam = null;
-var teamId = null;
-var skaterId = null;
-var penaltyId = null;
-var fo_exp = null;
 var nrows = 5;
-var penalties = {1: {}, 2: {}};
+var totalPenalties = {1: 0, 2: 0}
 
 function initialize() {
 // Run when the screen is loaded
 	
-	// From /json/WS.js
+	totalPenalties = {1: 0, 2: 0}
+	
 	WS.Connect();
 	WS.AutoRegister();
 
 	// For each team register connections for the name, alternate name, and colors
 	$.each([1, 2], function(idx, t) {
-		WS.Register([ 'ScoreBoard.Team(' + t + ').Name' ]); 
-		WS.Register([ 'ScoreBoard.Team(' + t + ').AlternateName' ]);
+		WS.Register( [ 'ScoreBoard.Team(' + t + ').Name' ], function(k, v) { setTeamName(t); }); 
+		WS.Register([ 'ScoreBoard.Team(' + t + ').AlternateName' ], function(k,v) { setTeamName(t) });
 		WS.Register([ 'ScoreBoard.Team(' + t + ').Color' ], function(k, v) { 
 			$('.Team' + t + 'custColor').css('color', WS.state['ScoreBoard.Team(' + t + ').Color(overlay_fg)']); 
 			$('.Team' + t + 'custColor').css('background-color', WS.state['ScoreBoard.Team(' + t + ').Color(overlay_bg)']); 
@@ -39,27 +35,12 @@ function initialize() {
 	
 }
 
-function adjust(which, inc) {
-	var elem = $(".PenaltyEditor ." + which);
-	console.log(elem, elem.val(), inc);
-	elem.val(parseInt(elem.val()) + inc);
-}
-
-function clear() {
-	console.log(penaltyId, skaterId);
-	if (penaltyId == null || skaterId == null) {
-		penaltyEditor.dialog("close");
-	} else {
-		WS.Command("Penalty", { teamId: teamId, skaterId: skaterId, penaltyId: penaltyId, fo_exp: fo_exp, jam: 0, period: 0 });
-		penaltyEditor.dialog('close');
-	}
-}
-
 var skaterIdRegex = /Skater\(([^\)]+)\)/;
 var penaltyRegex = /Penalty\(([^\)]+)\).Code/;
 function skaterUpdate(t, k, v) { 
-	// Called when ScoreBoard.Team(t).Skater changes
-	// Should recreate the table by adding the penalty 
+// Called when ScoreBoard.Team(t).Skater changes
+// Checks to see if the change is to skater's information or to a penalty
+// If a penalty, call displayPenalty to add it to the board
 	
 	// Extract skater ID from k.  Quit if no match.
 	match = k.match(skaterIdRegex); 
@@ -71,15 +52,12 @@ function skaterUpdate(t, k, v) {
 	//Example: prefix = ScoreBoard.Team(1).Skater(id)
 	
 	if (k == prefix + '.Number') {
-		// This branch fires when the view is launched or when a new game is started.
+		// This branch only fires when the view is launched or when a new game is started.
 		
 		// If the value is null, return
 		if (v == null) {
 			return;
 		}
-		
-		// Populate team name and colors
-		setTeamName(t);
 		
 	} else {  
 		// If the key indicates that this is NOT a skater number, we check to see if it's a 
@@ -91,97 +69,91 @@ function skaterUpdate(t, k, v) {
 			return;
 		var p = match[1];
 		
-		var name = WS.state[prefix + '.Name']
-		var number = WS.state[prefix + '.Number']
 		// Add penalty to display
-		displayPenalty(t, id, p, name, number);
+		displayPenalty(t, id, p);
 
 	}
 }
 
-function displayPenalty(t, s, p, name, number) { 
+function displayPenalty(t, s, p) { 
 	// Given a team, a skater ID, penalty number, name, and number, update the table.
 	
-	var priorNumber = '';
-	var priorName = '';
-	var priorCode = '';
-	var priorTotal = '';
+	var prefix = 'ScoreBoard.Team(' + t + ').Skater(' + s + ').Penalty(' + p + ')';
+	var period = WS.state[prefix + '.Period'];
+	var jam = WS.state[prefix + '.Jam'];
+	var code = WS.state[prefix + '.Code'];
+	var name = WS.state['ScoreBoard.Team(' + t + ').Skater(' + s + ').Name']
+	var number = WS.state['ScoreBoard.Team(' + t + ').Skater(' + s + ').Number']
+	var teamTable = $('.Team' + t + ' tbody');
+	var rowPeriod;
+	var rowJam;
+	var row;
+	var newRow
 	
-	// Define elements to change
-	var numberBox = $('.Team' + t + ' .Row[id=Row' + t + '0] .Number')
-	var nameBox = $('.Team' + t + ' .Row[id=Row' + t + '0] .Name')
-	var codeBox = $('.Team' + t + ' .Row[id=Row' + t + '0] .Code')
-
-	// Repopulate penalty array for this skater
-	penalties[t][s] = getPenalties(t,s);
-	code = penalties[t][s][p].code;
-	var total = Object.keys(penalties[t][s]).length;
+	//TODO - Make foul outs and expulsions pretty.
 	
-	if (code != null) {
-	// If the code exists, update the penalty box data and text
-		
-		for (var row = 4; row > 0 ; row--){
-			priorName = $('.Team' + t + ' .Row[id=Row' + t + String(row-1) + '] .Name').text();
-			$('.Team' + t + ' .Row[id=Row' + t + String(row) + '] .Name').text(priorName);
-			priorCode = $('.Team' + t + ' .Row[id=Row' + t + String(row-1) + '] .Code').text();
-			$('.Team' + t + ' .Row[id=Row' + t + String(row) + '] .Code').text(priorCode);	
-			priorNumber = $('.Team' + t + ' .Row[id=Row' + t + String(row-1) + '] .Number').text();
-			$('.Team' + t + ' .Row[id=Row' + t + String(row) + '] .Number').text(priorNumber);
+	if (code == null){
+	// If a penalty is cleared, remove it from the table and append a blank line at the end.
+	// Regenerating the entire table might be an option, but this is probably preferable,
+	// given that there's no way to enforce same jam penalties going back up in the right order.
+		var clearMatch = teamTable.find('tr')
+			.filter(function() {return $(this).data("id") == s && $(this).data("penalty") == p})
+		if (clearMatch.size() > 0) {
+			clearMatch.remove();
+			teamTable.append($("<tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>"));
+			totalPenalties[t] -= 1;
+			setTeamName(t);
 		}
-		// Store the period and the jam for the penalty as attributes of the row for sorting.
-		$('.Team' + t + ' .Row[id=Row' + t + String(row) + ']').data('period',penalties[t][s][p].period);
-		$('.Team' + t + ' .Row[id=Row' + t + String(row) + ']').data('jam',penalties[t][s][p].jam);
-		nameBox.text(name)
-		numberBox.text(number);
-		codeBox.text(code + ' (' + total + ')');
-
-		
-	} else {
-	// If the code is blank, (i.e., someone cleared a code) do nothing
-	// TODO - figure out a way to remove the line from the display in this case.
-		return;
+		return
 	}
+	
+	for (var n = 1; n <= nrows; n++){
+	// If this penalty is more recent than any of the penalties in the list, insert it
+	// in the list at that point. (starting from the top)
+		row = teamTable.find('tr:eq(' + n + ')')
+		rowPeriod = row.data().period
+		rowJam = row.data().jam
+		if (rowPeriod == undefined
+			|| period > rowPeriod 
+			|| (period == rowPeriod && jam >= rowJam)){
+				newRow = $('<tr>').addClass('Penalty').data({
+					"period": period, 
+					"jam": jam,
+					"id": s,
+					"penalty": p
+			});
+			newRow.append($('<td>').addClass('Number').text(number));
+			newRow.append($('<td>').addClass('Name').text(name));
+			newRow.append($('<td>').addClass('Code').text(code + ' (' + p + ')'));
+			row.before(newRow);
+			teamTable.find('tr:last').remove();
+			totalPenalties[t] += 1;
+			setTeamName(t);
+			return;
+		}
+	}
+	
+	
+	return;
 
 }
 
 function setTeamName(t) {
-	// Populate team name and colors
+	// Populate team name
 	
 	var team = $('.Team' + t + ' tbody');
 	var head = document.getElementById('head' + t);
 	
-	
-	// Retrieve team name and colors from websocket
+	// Retrieve team name from websocket
 	var teamName = WS.state['ScoreBoard.Team(' + t + ').Name'];
-	var teamFColor = WS.state['ScoreBoard.Team(' + t + ').Color(overlay_fg)'];
-	var teamBColor = WS.state['ScoreBoard.Team(' + t + ').Color(overlay_bg)'];	
 	if (WS.state['ScoreBoard.Team(' + t + ').AlternateName(whiteboard)'] != null) {
 		teamName = WS.state['ScoreBoard.Team(' + t + ').AlternateName(whiteboard)']
 	}
 
 	// Redo the header line for the team name.
-	head.innerHTML = '<span class="Team' + t + 'custColor"; style="font-size: 200%;">' + teamName + '</span>';
-	$('.Team' + t + 'custColor').css('color', teamFColor);
-	$('.Team' + t + 'custColor').css('background-color', teamBColor);	
+	head.innerHTML = '<span class="Team' + t + 'custColor"; style="font-size: 200%;">' + teamName + ' - ' + totalPenalties[t] +  '</span>';
 }
 
-function getPenalties(t, id) {
-// Given a team and skater ID, add their penalties to the penalty array.
-	var prefix = 'ScoreBoard.Team(' + t + ').Skater(' + id + ').Penalty(';
-	var skaterPenalties = {};
-	var code = '';
-	
-	for (var n=1; n < 10; n++){
-		code = WS.state[prefix + n + ').Code']
-		if (code == undefined){
-			return skaterPenalties;
-		}
-		skaterPenalties[n] = {
-				code: code,
-				period: WS.state[prefix + n + ').Period'],
-				jam: WS.state[prefix + n + ').Jam']
-		}
-	}
-	return skaterPenalties;
-}
+
+
 
