637e304cdcefb0716d196b1efb9ba7119c951102
AdamSmasherDerby <danielalt7@gmail.com>
2018-07-25 22:53:19 -0400

    Number of jams might be working.

--- html/views/announcer/jammerstats.js
+++ html/views/announcer/jammerstats.js
@@ -2,8 +2,11 @@ $(initialize);
 
 var period = null;
 var jam = null;
+var jammerList = {};
 
 function initialize() {
+	jammerList = {};
+	
 	WS.Connect();
 	WS.AutoRegister();
 
@@ -18,7 +21,60 @@ function initialize() {
 	WS.Register( [ 'ScoreBoard.Clock(Period).Number' ], function(k, v) { period = v; });
 	WS.Register( [ 'ScoreBoard.Clock(Jam).Number' ], function(k, v) { jam = v; });
 
-	WS.Register( [ 'ScoreBoard.Team(1).Skater' ], function(k, v) { skaterUpdate(1, k, v); } ); 
-	WS.Register( [ 'ScoreBoard.Team(2).Skater' ], function(k, v) { skaterUpdate(2, k, v); } ); 
+	WS.Register( [ 'ScoreBoard.Team(1).Skater' ]); 
+	WS.Register( [ 'ScoreBoard.Team(2).Skater' ]); 
 
+	WS.Register( [ 'Game' ], function(k,v){
+		processEvent(k,v);
+	})
+}
+
+var jammerRegexp = /Game\.Period\(\d\)\.Jam\(\d\)\.Team\((\d)\)\.Skater\(Jammer\)\.Id/;
+
+function processEvent(k, v) {
+	var prefix = '';
+	var team = '';
+	var teamTable;
+	console.log(k,v);
+	
+	// If this is a jammer, add them to the list
+	var match = k.match(jammerRegexp);
+	if (match != null && v != null){
+		team = match[1];
+		teamTable = $('.Team' + team + ' tbody');
+		prefix = 'ScoreBoard.Team(' + team + ').Skater(' + v + ')';
+
+		if (!jammerList.hasOwnProperty(v)){
+		// If this is a new jammer, add them to the jammer list, and add a row to the display
+			jammerList[v] = {
+				name: WS.state[prefix + '.Name'],
+				number: WS.state[prefix + '.Number'],
+				team: team
+			}
+			console.log(jammerList);
+			teamTable.append(makeJammerRow(v));
+		}
+		
+		var jammerRow = teamTable.find('tr').filter(function() {return $(this).data('number') == jammerList[v].number})
+		console.log(WS.state['Game.Period(' + period + ').Jam(' + jam + ').JamLength']);
+		if (WS.state['Game.Period(' + period + ').Jam(' + jam + ').JamLength'] == '0:00') {
+			console.log('here');
+			jammerRow.find('td.Jams').html(+jammerRow.find('td.Jams').html() + 1);
+		}
+		
+		}
+}
+
+function makeJammerRow(id) {
+// Given a jammer ID, return a row for the table
+	var row = $('<tr>').addClass('Jammer').data('number', jammerList[id].number);
+	row.append($('<td>').addClass('Name').html(jammerList[id].name + ' (' + jammerList[id].number + ')'));
+	row.append($('<td>').addClass('Jams').html('0'));
+	row.append($('<td>').addClass('Lead').html('0'));
+	row.append($('<td>').addClass('LeadPct').html('0'));
+	row.append($('<td>').addClass('Box').html('0'));
+	row.append($('<td>').addClass('Pts').html('0'));
+	row.append($('<td>').addClass('Dif').html('0'));
+	
+	return row;
 }
